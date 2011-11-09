5a0371677564b791a565b0ba3f80e6b42a692cba
Leonardo Olivares <leonardo311@gmail.com>
2011-11-09 03:29:24 -0300

    Corecciones menores

--- bd/conexion.class.php
+++ bd/conexion.class.php
@@ -8,15 +8,13 @@
 		private $conn;
 		private static $instance;
 		
-		public function __construct()  
-		{  
+		public function __construct() {
 			$this->conn=mysql_connect(SERVIDOR,USUARIO,PASS) 
 									or die("Imposible conectar con ". SERVIDOR);  
 			mysql_select_db(BASE,$this->conn) or die(BASE." no existe...");  
 		}
 		
-		public static function getInstance()
-		{
+		public static function getInstance() {
 			if (!isset(self::$instance)) {
 				$c = __CLASS__;
 				self::$instance = new $c;
@@ -24,29 +22,24 @@
 			return self::$instance;
 		}
 		
-		public function __clone()
-		{
+		public function __clone() {
 			trigger_error('Clone no se permite.', E_USER_ERROR);
 		}
 		
-		public function __destruct()  
-		{  
+		public function __destruct() {
 			mysql_close($this->conn);  
 		}  
 		
-		public function consulta($sql)  
-		{  
+		public function consulta($sql) {
 			return mysql_query($sql,$this->conn);  
 		}  
 		
-		public function elemento($id)  
-		{  
+		public function siguiente($id) {
 			$line=mysql_fetch_array($id);  
 			return $line;  
 		}  
 		
-		public function mensajeError()  
-		{  
+		public function mensajeError() {
 			return mysql_error($this->conn);  
 		}  
 		
